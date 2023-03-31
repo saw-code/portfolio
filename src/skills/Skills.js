54ed6f4c7171a4b4d56c727362c440d6a34fbefe
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-03-31 09:04:46 +0300

    Add Skill component

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './Skills.module.css';
+import styleContainer from '../common/styles/Container.module.css'
 
 const Skills = () => {
   return (
     <div className={style.skillsBlock}>
-      
+      <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+        <h2 className={style.title}>Skills</h2>
+        <div className={style.skills}></div>
+      </div>
     </div>
   );
 };
