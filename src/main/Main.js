4b6ee39e0bdea13b71e992e289950f4d45f536cf
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-03-31 08:28:36 +0300

    add Skills component

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './Main.module.css';
-
+import styleContainer from '../common/styles/Container.module.css'
 
 const Main = () => {
   return (
     <div className={style.mainBlock}>
-      <div className={style.container}>
+      <div className={styleContainer.container}>
         <div className={style.text}>
           <span>Hi There</span>
           <h1>Дмитрий Пилявский</h1>
---
+++ src/skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Skills.module.css';
+
+const Skills = () => {
+  return (
+    <div className={style.skillsBlock}>
+      
+    </div>
+  );
+};
+
+export default Skills;
