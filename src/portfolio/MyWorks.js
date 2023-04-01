57b54ae74802e2893c3208a34fc91fad7cc60d15
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-01 11:23:47 +0300

    Add MyWorks component

---
+++ src/portfolio/MyWorks.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from "../skills/Skills.module.css";
+import styleContainer from "../common/styles/Container.module.css";
+
+const MyWorks = () => {
+  return (
+    <div className={style.skillsBlock}>
+      <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+        <h2 className={style.title}>Skills</h2>
+        <div className={style.skills}>
+          <Portfolio title="JS" description={text}/>
+          <Portfolio title="CSS" description={text}/>
+          <Portfolio title="React" description={text}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyWorks;
